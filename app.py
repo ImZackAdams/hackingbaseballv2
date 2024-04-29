cdefb781a2a28530854ae4e6b3123d9f5e3dd993
Zack Adams <imzackadams@protonmail.com>
2024-04-29 03:19:46 -0400

    routes experiment

--- app.py
+++ app.py
@@ -1,18 +1,37 @@
 from flask import Flask, render_template
 from datetime import datetime
+from config import Config
+from flask_caching import Cache
+# Import SQLAlchemy and Migrate here
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 # Import the game_management blueprint
-from modules.game_management import game_management
+from modules.game_management.routes import game_management_bp
 # Import the get_or_update_schedules function from the utils module within game_management
 from modules.game_management.utils import get_or_update_schedules
 
 app = Flask(__name__)
+
+# Configure the Flask app (you can separate this out into different environment configurations if needed)
+app.config.from_object(Config)
+
+# Set up caching
+cache = Cache(app)
+cache.init_app(app)
+
+# Set up the database
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 # Register the blueprint from the game_management module
-app.register_blueprint(game_management, url_prefix='/games')
+app.register_blueprint(game_management_bp, url_prefix='/games')
 
 
 @app.route("/")
+@cache.cached(timeout=86400)  # Cache this page for one day
 def index():
-    year = 2024
+    year = app.config.get('CURRENT_YEAR', datetime.now().year)
     today = datetime.today().date()
     games_today = get_or_update_schedules(year)
     games_today = games_today[games_today['Date'].dt.date == today]
@@ -30,7 +49,6 @@ def index():
     else:
         return render_template("index.html", games=games)
 
-
-# The app.run() call is only necessary when running the application directly and not when deploying to a production server
+# Only for local development!
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=app.config.get('DEBUG', False))
