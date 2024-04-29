098ae5c4eb498197600f2b5423690a6c9fb7189c
Zack Adams <imzackadams@protonmail.com>
2024-04-29 09:02:02 -0400

    cleanup unecessary comments in app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from modules.game_management.routes import game_management
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hacking_baseball.db'
 # db.init_app(app)
-#
+
 app.register_blueprint(game_management)
-#
+
 # with app.app_context():
 #     db.create_all()
 
