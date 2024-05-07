a61f4bd332fad87423f83fd59a0f25d127722b85
Zack Adams <imzackadams@protonmail.com>
2024-05-07 11:56:09 -0400

    connect model to db

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from database import db
 from modules.game_management.routes import game_management
 from modules.result_display import results
+from modules.prediction.game_lineup import predict_game_outcome_from_lineup
+from modules.prediction.model import train_model
 
 app = Flask(__name__)
 
 app.register_blueprint(game_management)
-
 app.register_blueprint(results)
 
-#app.register_blueprint(prediction_bp)  # Register the prediction Blueprint
+# Train the model
+model = train_model()
+
+
+@app.route('/predict_game_outcome', methods=['POST'])
+def predict_game_outcome():
+    game_id = request.json['game_id']
+
+    predicted_outcome = predict_game_outcome_from_lineup(game_id, model)
+
+    return jsonify({'predicted_outcome': predicted_outcome.tolist()})
+
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hacking_baseball.db'
 # db.init_app(app)
