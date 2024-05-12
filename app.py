cf37c34d6c24479a9788b3b4e86c1ba37fb81925
Zack Adams <imzackadams@protonmail.com>
2024-05-11 22:56:23 -0400

    boiler plate for payment module

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from database import db
 from modules.game_management.routes import game_management
-from modules.result_display import results
+from modules.result_display import bp as result_display_bp
 from modules.prediction.game_lineup import predict_game_outcome_from_lineup
 from modules.prediction.model import train_model
-from payment import create_checkout_session
+from modules.payment_processing import create_checkout_session, app as payment_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(game_management)
-app.register_blueprint(results)
+app.register_blueprint(result_display_bp)
+app.register_blueprint(payment_bp)
 
 # Train the model
 model = train_model()
@@ -22,6 +23,18 @@ def predict_game_outcome():
     predicted_outcome = predict_game_outcome_from_lineup(game_id, model)
 
     return jsonify({'predicted_outcome': predicted_outcome.tolist()})
+
+
+@app.route('/success')
+def success():
+    # Render the success page with the user's results
+    return render_template('results.html')
+
+
+@app.route('/cancel')
+def cancel():
+    # Render the cancel page
+    return render_template('cancel.html')
 
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hacking_baseball.db'
