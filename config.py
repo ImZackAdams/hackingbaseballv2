a2e9f9b11ec4c3b09c6c78d74c92e6d640f88f1a
Zack Adams <imzackadams@protonmail.com>
2024-05-16 00:28:17 -0400

    delete test_app for debugging and unecessary config

--- config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-
-class Config(object):
-    CURRENT_YEAR = 2024
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///path_to_your_database.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    CACHE_TYPE = 'simple'  # Consider "redis" for production use
-    # Add other global settings and configurations here
