df6f41089f9b7a9d80ac48f6498a5ec8fab73aac
Zack Adams <imzackadams@protonmail.com>
2024-08-06 10:10:41 -0400

    seperate lineup data collection from utils

--- modules/game_management/lineup.py
+++ modules/game_management/lineup.py
@@ -5,77 +5,7 @@ from pybaseball import schedule_and_record
 import requests
 import numpy as np
 
-# Define the cache file location
-cache_file = 'game_schedules.json'
-
-# Dictionary to map full team names to abbreviations
-team_name_to_abbreviation = {
-    'Arizona Diamondbacks': 'ARI', 'Atlanta Braves': 'ATL', 'Baltimore Orioles': 'BAL', 'Boston Red Sox': 'BOS',
-    'Chicago Cubs': 'CHC', 'Cincinnati Reds': 'CIN', 'Cleveland Guardians': 'CLE', 'Colorado Rockies': 'COL',
-    'Chicago White Sox': 'CHW', 'Detroit Tigers': 'DET', 'Houston Astros': 'HOU', 'Kansas City Royals': 'KC',
-    'Los Angeles Angels': 'LAA', 'Los Angeles Dodgers': 'LAD', 'Miami Marlins': 'MIA', 'Milwaukee Brewers': 'MIL',
-    'Minnesota Twins': 'MIN', 'New York Mets': 'NYM', 'New York Yankees': 'NYY', 'Oakland Athletics': 'OAK',
-    'Philadelphia Phillies': 'PHI', 'Pittsburgh Pirates': 'PIT', 'San Diego Padres': 'SD', 'Seattle Mariners': 'SEA',
-    'San Francisco Giants': 'SF', 'St. Louis Cardinals': 'STL', 'Tampa Bay Rays': 'TB', 'Texas Rangers': 'TEX',
-    'Toronto Blue Jays': 'TOR', 'Washington Nationals': 'WSN'
-}
-
-
-def fetch_and_process_schedules(year):
-    team_abbreviations = [
-        'ARI', 'ATL', 'BAL', 'BOS', 'CHC',
-        'CIN', 'CLE', 'COL', 'CHW', 'DET',
-        'HOU', 'KC', 'LAA', 'LAD', 'MIA',
-        'MIL', 'MIN', 'NYM', 'NYY', 'OAK',
-        'PHI', 'PIT', 'SD', 'SEA', 'SF',
-        'STL', 'TB', 'TEX', 'TOR', 'WSN'
-    ]
-
-    all_games = pd.DataFrame()
-
-    for team in team_abbreviations:
-        try:
-            team_schedule = schedule_and_record(year, team)
-            all_games = pd.concat([all_games, team_schedule], ignore_index=True)
-        except Exception as e:
-            print(f"Failed to retrieve schedule for {team}: {e}")
-
-    all_games = all_games.dropna(subset=['Date', 'Tm', 'Opp'])
-    all_games['unique_id'] = all_games.apply(lambda row: row['Date'] + ''.join(sorted([row['Tm'], row['Opp']])), axis=1)
-    unique_games = all_games.drop_duplicates(subset=['unique_id'])
-    unique_games = unique_games.drop(columns=['unique_id'])
-    unique_games['Date'] = pd.to_datetime(unique_games['Date'], errors='coerce', format='%A, %b %d')
-    unique_games['Date'] = unique_games['Date'].apply(lambda d: d.replace(year=year) if not pd.isnull(d) else d)
-    unique_games_sorted = unique_games.sort_values(by='Date', ascending=True)
-    unique_games_sorted = unique_games_sorted.reset_index(drop=True)
-    unique_games_sorted['id'] = unique_games_sorted.apply(
-        lambda row: f"{row['Tm']}_{row['Opp']}_{row['Date'].strftime('%Y%m%d')}" if not pd.isnull(
-            row['Date']) else None, axis=1
-    )
-    unique_games_sorted['Attendance'].replace(r'^Unknown$', np.nan, regex=True,
-                                              inplace=True)  # Convert 'Unknown' to NaN
-
-    return unique_games_sorted
-
-
-def get_or_update_schedules(year):
-    if os.path.exists(cache_file):
-        modified_time = datetime.fromtimestamp(os.path.getmtime(cache_file))
-        if datetime.now() - modified_time < timedelta(days=1):
-            with open(cache_file, 'r') as file:
-                schedules = pd.read_json(file, convert_dates=['Date'])
-                if 'id' not in schedules.columns:
-                    schedules['id'] = schedules.apply(
-                        lambda row: f"{row['Tm']}_{row['Opp']}_{row['Date'].strftime('%Y%m%d')}" if not pd.isnull(
-                            row['Date']) else None, axis=1
-                    )
-                schedules['Attendance'].replace(r'^Unknown$', np.nan, regex=True,
-                                                inplace=True)  # Convert 'Unknown' to NaN
-                return schedules
-
-    schedules = fetch_and_process_schedules(year)
-    schedules.to_json(cache_file, date_format='iso')
-    return schedules
+from modules.game_management.utils import team_name_to_abbreviation
 
 
 def fetch_starting_lineups(date):
